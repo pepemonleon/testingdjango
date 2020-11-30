7005775ce3aa1556281839e9d5e089424a388c7f
Pepe Monleón <jose.monleon@onestic.com>
2020-11-30 16:06:20 +0100

    add some files missed

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
     posts = Post.objects.filter(Q(published_date__lte=timezone.now()) & ~Q(title='Sample title 2')).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
