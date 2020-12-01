d1405ca0e42f0da70e3a7b100629191f1a83cc10
Pepe Monleón <jose.monleon@onestic.com>
2020-12-01 16:05:32 +0100

    add comments feature

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post_new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 
+# Connect models (models.py) with templates (post_list.html)
 
 
 
 def post_list(request):
+    '''
+    recovery all the items in the table Post (is inside of the database. Check models.py) with that specifications.
+    Then render (print) the blog/post_list.html and recive the posts parameter
+    '''
     posts = Post.objects.filter(Q(published_date__lte=timezone.now()) & ~Q(title='Sample title 2')).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
+    '''
+    recovery just one item in the table Post (is inside of the database. Check models.py) with that primary key.
+    Then render (print) the blog/post_detail.html and recive the posts parameter
+    '''
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
@@ -43,3 +52,26 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
